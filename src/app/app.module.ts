8fe2f2878333f5c90ab78eb0b2815e87f02d27ab
Yaroslava Kalashnyk <Yaroslava_Kalashnyk@epam.com>
2018-06-21 15:00:19 +0300

    update npm and create two components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
